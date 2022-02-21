19e96a98d085b6a69bdefbe2609dce7a35823512
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-02-21 17:59:44 -0500

    added actual use of state to my nabar changed the text color and made the navbar sticky

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Navbar from './components/navbar/navbar'
-import Typical from './components/autoTyper/autoTyper'
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Typical from "./components/autoTyper/autoTyper";
 function App() {
   return (
     <div className="App">
-      <header className="port-header">
-      <Navbar />
+      <header className="nav-header">
+        <Navbar />
       </header>
       <main>
-      <h1 className='auto-typer-container'>
-        <Typical/>
-      </h1>
+        <h1 className="auto-typer-container">
+          <Typical />
+        </h1>
       </main>
     </div>
   );
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,30 +1,41 @@
-import react from 'react';
-import {MenuItems} from './MenuItems'
-import './Navbar.css'
-class Navbar extends react.Component{
-    state= { clicked: false}
-    // functions inside the constructor function have to be an arrow function
-    // setState()
-    handleClick= () =>{
-        this.setState({ clicked: !this.state.clicked})
-    }
-    render(){
-        return(
-            <nav className='navbar-container'>
-                <div className='logo'> <a className='logo-a' href='#About Me'>Brandon Diaz</a></div>
-                <div className='menu-icon' onClick={this.handleClick}> <i className={this.state.clicked ? 'lni lni-cross-circle' : 'lni lni-menu'}></i></div>
-                <ul className={this.state.clicked ? 'nav-ul active' : 'nav-ul'}>
-                    {MenuItems.map((item, index) =>{
-                        return(
-                            // make sure li always has a key
-                            <li key={index}><a className={item.cName} href={item.url}>{item.name}</a></li>
-                        )
-                    })}
-                </ul>
-            </nav>
-        )
-    }
+// make sure to captialize react when importing react when using a funciton and return
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
+// make sure to just use a function when creating components that way you can actually use proper vars inside to create more complex html elements
+function Navbar() {
+  // setting the state of the mobile navbar menu
+  const [clicked, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!clicked);
+  };
+  return (
+    <nav className="navbar-container">
+      <div className="logo">
+        {" "}
+        <a className="logo-a" href="#About Me">
+          Brandon Diaz
+        </a>
+      </div>
+      <div className="menu-icon" onClick={handleClick}>
+        {" "}
+        <i className={clicked ? "lni lni-cross-circle" : "lni lni-menu"}></i>
+      </div>
+      <ul className={clicked ? "nav-ul active" : "nav-ul"}>
+        {MenuItems.map((item, index) => {
+          return (
+            // make sure li always has a key
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
 // make sure to always export
-export default Navbar
+export default Navbar;
