82e70c86f33dff51f734521a3afbad1558b967e4
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-02-25 14:55:10 -0500

    added content to the app.js and also cleaned up the app.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Typical from "./components/autoTyper/autoTyper";
+import Footer from "./components/footer/footer";
+import AboutMe from "./components/pages/aboutMeSection/aboutMe";
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="Home">
       <header className="nav-header">
         <Navbar />
       </header>
+      <h1 className="auto-typer-container">
+        <Typical />
+      </h1>
       <main>
-        <h1 className="auto-typer-container">
-          <Typical />
-        </h1>
+        <AboutMe />
       </main>
+      <Footer />
     </div>
   );
 }
